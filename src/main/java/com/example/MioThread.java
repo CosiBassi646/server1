bd363fca6ef942afd15e2cc808cf2fc22de5dd90
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 10:42:27 +0200

    aggiunta menu

--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -10,12 +10,14 @@ class MioThread extends Thread implements Runnable{
     private Socket socket;
     private BufferedReader in;
     private DataOutputStream out;
+    private BufferedReader inScelta;
 
     public MioThread(Socket socket) {
         this.socket = socket;
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new DataOutputStream(socket.getOutputStream());
+            inScelta = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
             System.err.println("Errore nell'inizializzazione degli stream: " + e.getMessage());
         }
@@ -23,23 +25,56 @@ class MioThread extends Thread implements Runnable{
 
     @Override
     public void run() {
-        String stringaRicevuta;
+        String stringaRicevuta = "";
+        String scelta = "";
 
+        // Leggi la stringa inviata dal client
         try {
+            stringaRicevuta = in.readLine();   
+            scelta = inScelta.readLine(); 
             do {
-                // Leggi la stringa inviata dal client
-                stringaRicevuta = in.readLine(); // Legge fino a un carattere di nuova linea
+                switch (scelta) {
+                    case "1":// Trasforma la stringa in maiuscolo e la invia al client
+                    stringaRicevuta = in.readLine();   
+                    String stringaMaiuscola = stringaRicevuta.toUpperCase();
+                    out.writeBytes(stringaMaiuscola + "\n"); // Invia la risposta con una nuova linea   
+                    scelta = inScelta.readLine();   
+                    break;
+                
+                    case "2"://trasfortma la stringa in minuscolo
+                    stringaRicevuta = in.readLine();   
+                    String stringaMinuscola = stringaRicevuta.toLowerCase();
+                    out.writeBytes(stringaMinuscola + "\n");//invio risposta
+                    scelta = inScelta.readLine();   
+                    break;
+              
+                    case "3"://mostra la stringa letta al contrario
+                    stringaRicevuta = in.readLine();   
+                    String reversedStr = "";
+                    for (int i = 0; i < stringaRicevuta.length(); i++) {
+                    reversedStr = stringaRicevuta.charAt(i) + reversedStr;
+                    }
+                    out.writeBytes(reversedStr);
+                    scelta = inScelta.readLine();   
+                    break;
 
-                if (stringaRicevuta == null || stringaRicevuta.equals("!")) {
-                    break; // Esci se il client ha chiuso la connessione o ha inviato '!'
+                    case "4"://conta il numero di caratteri che compongono la stringa
+                    stringaRicevuta = in.readLine();   
+                    int lungStringa = stringaRicevuta.length();
+                    out.writeBytes("LUNGHEZZA DELLA STRINGA: " + lungStringa + "\n");   
+                    scelta = inScelta.readLine();   
+                    break;
+
+
+
+                    case "0":
+                        break;
+                    
+                    default:
+                        break;
                 }
-
-                System.out.println("La stringa ricevuta è: " + stringaRicevuta);
-
-                // Trasforma la stringa in maiuscolo e la invia al client
-                String stringaMaiuscola = stringaRicevuta.toUpperCase();
-                out.writeBytes(stringaMaiuscola + "\n"); // Invia la risposta con una nuova linea
-            } while (true);
+                     
+            } while (scelta.equals("0") == false);
         } catch (IOException e) {
             System.err.println("Errore nella comunicazione con il client: " + e.getMessage());
         } finally {
